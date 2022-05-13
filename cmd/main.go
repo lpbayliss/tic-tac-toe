906227cf22c52f8226e4a54ecc72ab16886fb1be
Luke Bayliss <lukepbayliss@gmail.com>
2022-05-13 20:21:24 +1000

    Add more things

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/lpbayliss/tic-tac-toe/game"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -9,9 +14,24 @@ const (
 )
 
 type GameServer struct {
-
+	pb.UnimplementedGameServer
 }
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Starting Game Server!")
+
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGameServer(s, &GameServer{})
+
+	log.Printf("Server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
